ab1b079e6d6615ddba1131a63fc263a36e29d6ce
Godmave <godmave@gmail.com>
2018-11-11 12:10:53 +0100

    #escape pattern

--- lua/train_tracking.lua
+++ lua/train_tracking.lua
@@ -336,6 +336,10 @@ local function deserialize_train(station, data)
     end
 end
 
+local function escape_pattern(text)
+    return text:gsub("([^%w])", "%%%1")
+end
+
 local function deserialize_train_schedule(train, schedule)
     if schedule == nil then
         return
@@ -345,7 +349,7 @@ local function deserialize_train_schedule(train, schedule)
         local myInstanceName = trainStopTrackingApi.lookupIdToServerName(0)
         for _, record in ipairs(schedule.records) do
             -- remove the @ instanceName from local stations
-            if record.station:match("@ " .. myInstanceName) then
+            if record.station:match("@ " .. escape_pattern(myInstanceName)) then
                 record.station = record.station:match("^(<CT?[0-9%+]*> .*) @")
             end
         end
