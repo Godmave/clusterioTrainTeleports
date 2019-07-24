a89de85c57167490cfbd72d10d8d37089ed14a6c
Godmave <godmave@gmail.com>
2019-07-24 22:43:07 +0200

    #remove debug stuff

--- index.js
+++ index.js
@@ -5,22 +5,39 @@ const fs = require("fs");
 const COMPRESS_LUA = false;
 
 module.exports = class remoteCommands {
-    constructor(mergedConfig, messageInterface, extras){
-        this.messageInterface = messageInterface;
-        this.config = mergedConfig;
-        this.socket = extras.socket;
+	constructor(mergedConfig, messageInterface, extras){
+		this.messageInterface = messageInterface;
+		this.config = mergedConfig;
+		this.socket = extras.socket;
         this.registered = false;
 
         this.trainstopDB = {};
         this.zonesDB = {};
 
-        // initialize mod with Hotpatch
-        (async () => {
-            let startTime = Date.now();
-            let hotpatchInstallStatus = await this.checkHotpatchInstallation();
-            this.messageInterface("Hotpach installation status: "+hotpatchInstallStatus);
-            if(hotpatchInstallStatus){
-                var returnValue;
+		let socketRegister = () => {
+			this.socket.emit("registerTrainTeleporter", {
+				instanceID: this.config.unique,
+			});
+
+            // no need for the setTimeout if the remote function is not just getting added via hotpatch
+            let initInterval = setInterval(async () => {
+                if(this.registered) {
+                    this.messageInterface('/silent-command remote.call("trainTeleports", "setWorldId","' + this.config.unique + '")');
+                    this.messageInterface('/silent-command remote.call("trainTeleports", "init")');
+                    clearInterval(initInterval)
+                }
+            }, 5000);
+        };
+		
+		this.socket.on("hello", () => socketRegister());
+
+		// initialize mod with Hotpatch
+		(async () => {
+			let startTime = Date.now();
+			let hotpatchInstallStatus = await this.checkHotpatchInstallation();
+			this.messageInterface("Hotpach installation status: "+hotpatchInstallStatus);
+			if(hotpatchInstallStatus){
+				var returnValue;
                 var mainCode = await this.getSafeLua("sharedPlugins/trainTeleports/lua/trainTeleports.lua");
 
                 var trainCode = await this.getSafeLua("sharedPlugins/trainTeleports/lua/train_tracking.lua");
@@ -31,50 +48,24 @@ module.exports = class remoteCommands {
                 if(returnValue) console.log(returnValue);
 
 
-                this.messageInterface("trainTeleports installed in "+(Date.now() - startTime)+"ms");
-            } else {
-                this.messageInterface("Hotpatch isn't installed! Please generate a new map with the hotpatch scenario to use trainTeleports.");
-            }
-        })().catch(e => console.error(e));
+				this.messageInterface("trainTeleports installed in "+(Date.now() - startTime)+"ms");
+			} else {
+				this.messageInterface("Hotpatch isn't installed! Please generate a new map with the hotpatch scenario to use trainTeleports.");
+			}
+		})().catch(e => console.error(e));
 
-
-        let socketRegister = () => {
-            console.log("WAITING");
-            setTimeout(() =>function() {
-                console.log("REGISTERING");
-                this.socket.emit("DUMMYEMITTTTTTTTTTTTTTTTTT", {
-                    instanceID: this.config.unique,
-                });
-                this.socket.emit("registerTrainTeleporter", {
-                    instanceID: this.config.unique,
-                });
-            }, 2000);
-        };
-
-        this.socket.on("hello", () => socketRegister());
-
-        this.socket.on("disconnect", async () => {
-            console.log("LOST MASTER");
-            this.registered = false;
-            this.messageInterface('/silent-command remote.call("trainTeleports", "setWorldId","0") game.print("Lost connection to master!")');
+		this.socket.on("trainTeleporter_registered", async registered => {
+		    this.registered = true;
         });
 
-        this.socket.on("trainTeleporter_registered", async () => {
-            console.log("REGISTERED");
-            this.registered = true;
-            // this.messageInterface('/silent-command remote.call("trainTeleports","runCode", "game.print(\'≥\')")');
-            this.messageInterface('/silent-command remote.call("trainTeleports", "setWorldId","' + this.config.unique + '")');
-            this.messageInterface('/silent-command remote.call("trainTeleports", "init")');
-        });
-
-        this.socket.on("trainstopsDatabase", async trainstopsDB => {
-            console.log("got trainstops from master");
-            this.trainstopDB = trainstopsDB;
-            await this.applyTrainstopDB();
-        });
+		this.socket.on("trainstopsDatabase", async trainstopsDB => {
+		    console.log("got trainstops from master");
+		    this.trainstopDB = trainstopsDB;
+		    await this.applyTrainstopDB();
+		});
 
         this.socket.on("zonesDatabase", async zonesDB => {
-            console.log("got zones from master");
+             console.log("got zones from master");
             this.zonesDB = zonesDB;
             await this.applyZonesDB();
             this.messageInterface('/silent-command remote.call("trainTeleports","json","' + this.singleEscape(JSON.stringify({event: "instances", data: await clusterUtil.getInstances(this.config, zonesDB)})) + '")');
@@ -82,6 +73,7 @@ module.exports = class remoteCommands {
 
 
         this.socket.on("trainteleport_json", async data => {
+//            this.messageInterface('/silent-command remote.call("trainTeleports","runCode", "game.print(\'≥\')")');
             this.messageInterface('/silent-command remote.call("trainTeleports","json","' + this.singleEscape(JSON.stringify(data)) + '")');
         });
 
@@ -102,7 +94,7 @@ module.exports = class remoteCommands {
             this.messageInterface("/silent-command " + 'remote.call("trainTeleports", "updateStopInSchedules", "' +data.instanceID+ '", "'+this.doubleEscape(data.oldName)+'", "'+this.doubleEscape(data.name)+'")');
         });
 
-    }
+	}
 
     singleEscape(stop) {
         stop = stop.replace(/\\/g, "\\\\");
@@ -120,8 +112,8 @@ module.exports = class remoteCommands {
     }
 
 
-    async applyTrainstopDB(){
-        let trainstopsDB = this.trainstopDB;
+	async applyTrainstopDB(){
+	    let trainstopsDB = this.trainstopDB;
         let command = 'remote.call("trainTeleports", "runCode", "global.trainstopsData = {';
         for(let instanceID in trainstopsDB){
             command += '{id='+instanceID+',';
@@ -154,21 +146,18 @@ module.exports = class remoteCommands {
         //console.log(command);
         this.messageInterface("/silent-command "+command);
     }
-    async applyZonesDB(){
-        let data = {
-            event: "zones",
+	async applyZonesDB(){
+	    let data = {
+	        event: "zones",
             zones: this.zonesDB
         };
         this.messageInterface('/silent-command remote.call("trainTeleports","json","' + this.singleEscape(JSON.stringify(data)) + '")');
     }
-    async scriptOutput(data) {
-        if (!this.registered) {
-            return;
-        }
-        if(data !== null){
-            // this.messageInterface(data);
+	async scriptOutput(data){
+		if(data !== null){
+			// this.messageInterface(data);
 
-            if(data.substr(0,5) !== 'event') {
+			if(data.substr(0,5) !== 'event') {
                 this.socket.emit("trainteleport_json", JSON.parse(data));
             } else {
                 let parsedData = {};
@@ -204,53 +193,53 @@ module.exports = class remoteCommands {
                     this.messageInterface(`Unknown event: ` + parsedData.event);
                 }
             }
-        }
-    }
-    async getSafeLua(filePath){
-        return new Promise((resolve, reject) => {
-            fs.readFile(filePath, "utf8", (err, contents) => {
-                if(err){
-                    reject(err);
-                } else {
+		}
+	}
+	async getSafeLua(filePath){
+		return new Promise((resolve, reject) => {
+			fs.readFile(filePath, "utf8", (err, contents) => {
+				if(err){
+					reject(err);
+				} else {
                     // split content into lines
-                    contents = contents.split(/\r?\n/);
+					contents = contents.split(/\r?\n/);
 
-                    // join those lines after making them save again
-                    contents = contents.reduce((acc, val) => {
+					// join those lines after making them save again
+					contents = contents.reduce((acc, val) => {
                         val = val.replace(/\\/g ,'\\\\');
                         // remove leading and trailing spaces
-                        val = val.trim();
+					    val = val.trim();
                         // escape single quotes
-                        val = val.replace(/'/g ,'\\\'');
+					    val = val.replace(/'/g ,'\\\'');
 
-                        // remove single line comments
+					    // remove single line comments
                         let singleLineCommentPosition = val.indexOf("--");
                         let multiLineCommentPosition = val.indexOf("--[[");
 
-                        if(multiLineCommentPosition === -1 && singleLineCommentPosition !== -1) {
-                            val = val.substr(0, singleLineCommentPosition);
-                        }
+						if(multiLineCommentPosition === -1 && singleLineCommentPosition !== -1) {
+							val = val.substr(0, singleLineCommentPosition);
+						}
 
                         return acc + val + '\\n';
-                    }, ""); // need the "" or it will not process the first row, potentially leaving a single line comment in that disables the whole code
+					}, ""); // need the "" or it will not process the first row, potentially leaving a single line comment in that disables the whole code
 
-                    // console.log(contents);
+					// console.log(contents);
 
-                    // this takes about 46 ms to minify train_stop_tracking.lua in my tests on an i3
-                    if(COMPRESS_LUA) contents = require("luamin").minify(contents);
-
-                    resolve(contents);
-                }
-            });
-        });
-    }
-    async checkHotpatchInstallation(){
-        let yn = await this.messageInterface("/silent-command if remote.interfaces['hotpatch'] then rcon.print('true') else rcon.print('false') end");
-        yn = yn.replace(/(\r\n\t|\n|\r\t)/gm, "");
-        if(yn == "true"){
-            return true;
-        } else if(yn == "false"){
-            return false;
-        }
-    }
+					// this takes about 46 ms to minify train_stop_tracking.lua in my tests on an i3
+					if(COMPRESS_LUA) contents = require("luamin").minify(contents);
+					
+					resolve(contents);
+				}
+			});
+		});
+	}
+	async checkHotpatchInstallation(){
+		let yn = await this.messageInterface("/silent-command if remote.interfaces['hotpatch'] then rcon.print('true') else rcon.print('false') end");
+		yn = yn.replace(/(\r\n\t|\n|\r\t)/gm, "");
+		if(yn == "true"){
+			return true;
+		} else if(yn == "false"){
+			return false;
+		}
+	}
 };
